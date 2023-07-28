98b1708a0b2ef715c6815c5c7947eef96ed333e4
Kendall <kendallawills@gmail.com>
2023-07-28 17:05:59 -0400

    Step 6

---
+++ main.js
@@ -0,0 +1,11 @@
+const input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    //console.log(i);
+
+    for (let j = 0; j < vowels.length; j++) {
+        console.log(j);
+    }
+}
